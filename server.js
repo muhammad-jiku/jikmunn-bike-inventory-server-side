2dcba1a7e490717aa6deb19115869ac3c2477e16
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-02 16:23:17 +0600

    database added to the server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -11,6 +12,19 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello there! welcome');
+});
+
+// Database
+const uri = `mongodb+srv://${process.env.DB_AUTHOR}:${process.env.DB_PASSWORD}@cluster0.xapan.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db('bikeInventories').collection('inventories');
+  // perform actions on the collection object
+  client.close();
 });
 
 app.listen(port, () => {
